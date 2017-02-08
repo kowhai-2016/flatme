c0b583cc7e1c0241f900b94b49525b9455ca7b77
EDA <eda@mac-mini-4.local>
2017-02-09 11:34:43 +1300

    basic component layout of landing-page

--- src/client/components/Home/Landing/BannerRules.js
+++ src/client/components/Home/Landing/BannerRules.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 const BannerRules = () => {
   return (
-    <div className='container'>
-      <h1>"Make the Rules"</h1>
-      <p>contract generator at its finest</p>
+    <div className='height'>
+      <div className='container'>
+        <h1>"Make the Rules"</h1>
+        <p>contract generator at its finest</p>
+      </div>
     </div>
   )
 }
--- src/client/components/Home/Landing/Landing.js
+++ src/client/components/Home/Landing/Landing.js
@@ -1,13 +1,18 @@
 import React from 'react'
 
+import BannerDemo from './BannerDemo'
+import BannerDream from './BannerDream'
 import BannerHeader from './BannerHeader'
 import BannerRules from './BannerRules'
+
 
 const Landing = () => {
   return (
     <div>
       <BannerHeader />
       <BannerRules />
+      <BannerDemo />
+      <BannerDream />
     </div>
   )
 }
