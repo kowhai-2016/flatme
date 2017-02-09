2014bd29065c77d4c4f3857b85a6ec8c387410ed
amandahogan <hogan.ans@gmail.com>
2017-02-10 09:49:02 +1300

    fix navbar and work in progress sections

--- src/client/components/Home/Landing/BannerDream.js
+++ src/client/components/Home/Landing/BannerDream.js
@@ -3,7 +3,7 @@ import React from 'react'
 const BannerDream = () => {
   return (
     <div className='height'>
-      <div className='container'>
+      <div className='container leads'>
         <h1>"Live the Dream"</h1>
         <p>host a party, cook dinner, leaving for a long weekend, notify your flat</p>
       </div>
