35b9fb3b5b6622989f2d7091b26f35dc83b66d6c
David Christie <davidchristie.nz@gmail.com>
2017-02-10 12:48:59 +1300

    Phase out snapshot tests

--- test/client/components/App/App.test.js
+++ test/client/components/App/App.test.js
@@ -1,12 +1,13 @@
 /* eslint-env jest */
 
 import { shallow } from 'enzyme'
-import toJSON from 'enzyme-to-json'
 import React from 'react'
 
 import App from '../../../../src/client/components/App'
 
-test('App renders correctly', () => {
-  const wrapper = shallow(<App />)
-  expect(toJSON(wrapper)).toMatchSnapshot()
+describe('App', () => {
+  test('should render Navigation component', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.text().includes('Navigation')).toBe(true)
+  })
 })
--- test/client/components/Home/Home.test.js
+++ test/client/components/Home/Home.test.js
@@ -1,14 +1,19 @@
 /* eslint-env jest */
 
 import { shallow } from 'enzyme'
-import toJSON from 'enzyme-to-json'
 import React from 'react'
 
 import Home from '../../../../src/client/components/Home'
 
-const account = {}
-
-test('Home renders correctly', () => {
-  const wrapper = shallow(<Home account={account} />)
-  expect(toJSON(wrapper)).toMatchSnapshot()
+describe('Home', () => {
+  test('if logged out render Landing component', () => {
+    const account = {}
+    const wrapper = shallow(<Home account={account} />)
+    expect(wrapper.find('Landing').length).toBe(1)
+  })
+  test('if logged in render Dashboard component', () => {
+    const account = {user: {}}
+    const wrapper = shallow(<Home account={account} />)
+    expect(wrapper.find('Dashboard').length).toBe(1)
+  })
 })
