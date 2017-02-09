f0b8b1a9f949743883e409ea91e7f14ab336da3b
JessicaNeary <caneary@hotmail.com>
2017-02-09 16:35:29 +1300

    Shaped login form

--- src/client/reducers/index.js
+++ src/client/reducers/index.js
@@ -1,9 +1,16 @@
 import { routerReducer } from 'react-router-redux'
 import { combineReducers } from 'redux'
+import { combineForms } from 'react-redux-form'
 
 import account from './account'
 
+const loginCredentials = {
+  email: '',
+  password: ''
+}
+
 export default combineReducers({
+  forms: combineForms({ loginCredentials }, 'forms'),
   account,
   routing: routerReducer
 })
