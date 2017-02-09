eb29e3ea05b4cfcec469fb11291776bec1fa0b2d
amandahogan <hogan.ans@gmail.com>
2017-02-09 19:09:53 +1300

    deployed site to staging

--- migrations/20170209155043_tenancies.js
+++ migrations/20170209155043_tenancies.js
@@ -3,7 +3,7 @@ exports.up = knex => {
   return knex.schema.createTable('tenancies', table => {
     table.increments('id').primary()
     table.integer('flat_id').references('flats.id')
-    table.integer('user_id').references('users.id')    
+    table.integer('user_id').references('users.id')
   })
 }
 
