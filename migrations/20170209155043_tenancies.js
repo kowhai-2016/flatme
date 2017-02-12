a3c958e133f0f4c2991e89567d3f720a4c85ad33
amandahogan <hogan.ans@gmail.com>
2017-02-13 10:52:36 +1300

    add del cascade on db seeds

--- migrations/20170209155043_tenancies.js
+++ migrations/20170209155043_tenancies.js
@@ -2,8 +2,8 @@
 exports.up = knex => {
   return knex.schema.createTable('tenancies', table => {
     table.increments('id').primary()
-    table.integer('flat_id').references('flats.id')
-    table.integer('user_id').references('users.id')
+    table.integer('flat_id').references('flats.id').onDelete('CASCADE')
+    table.integer('user_id').references('users.id').onDelete('CASCADE')
   })
 }
 
