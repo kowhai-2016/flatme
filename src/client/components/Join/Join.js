99ebdb5606ef4fe8b08cfe006594052dc73ba015
Ti Bretin <ti-bretin@Tis-MacBook-Air.local>
2017-02-12 17:48:52 +1300

    Join page Styling

--- src/client/components/Join/Join.js
+++ src/client/components/Join/Join.js
@@ -18,184 +18,198 @@ const Join = React.createClass({
   },
   render () {
     return (
-      <Form model='forms.join'
-        onSubmit={this.handleSubmit}
-        validators={{
-          '': {
-            passwordMatch
-          }
-        }}
-      >
-        <div className='globalJoinForm'>
-          <div className='titleJoinForm'>
-            <Col sm={6} >
-              <label>First name:</label>
-            </Col>
-            <Col sm={6} >
-              <Control.text
-                className='input-group input-group-lg'
-                placeholder='Enter Your Name'
-                model='.firstName'
-                validators={{
-                  required,
-                  maxLength: maxLength(15)
+      <div className='container containerJoin'>
+        <Form model='forms.join'
+          onSubmit={this.handleSubmit}
+          validators={{
+            '': {
+              passwordMatch
+            }
+          }}
+        >
+
+        <div className='joinTitle'>
+            <Col sm={12} >
+            <h1>Join us</h1>
+          </Col>
+         </div>
+          <div className='globalJoinForm'>
+            <div className='titleJoinForm'>
+              <Col sm={4} >
+                <label>First name:</label>
+              </Col>
+              <Col sm={6}>
+                <Control.text
+                  className='input-group input-group-lg'
+                  placeholder='Enter Your Name'
+                  model='.firstName'
+                  validators={{
+                    required,
+                    maxLength: maxLength(15)
+                  }}
+                />
+              </Col>
+              <Errors
+                className='errors'
+                model='forms.join.firstName'
+                show='touched'
+                messages={{
+                  required: 'Required',
+                  maxLength: ' Must be 15 characters or less'
                 }}
               />
-            </Col>
-            <Errors
-              className='errors'
-              model='forms.join.firstName'
-              show='touched'
-              messages={{
-                required: 'Required',
-                maxLength: ' Must be 15 characters or less'
-              }}
-            />
-          </div>
+            </div>
 
-          <div className='titleJoinForm'>
-            <Col sm={6} >
-              <label>Last Name:</label>
-            </Col>
-            <Col sm={6} >
-              <Control.text
-                placeholder='Enter Your Family Name'
-                className='input-group input-group-lg'
-                model='.lastName'
-                validators={{
-                  required,
-                  maxLength: maxLength(15)
+            <div className='titleJoinForm'>
+              <Col sm={4} >
+                <label>Last Name:</label>
+              </Col>
+              <Col sm={6}>
+                <Control.text
+                  placeholder='Enter Your Family Name'
+                  className='input-group input-group-lg'
+                  model='.lastName'
+                  validators={{
+                    required,
+                    maxLength: maxLength(15)
+                  }}
+                />
+              </Col>
+              <Errors
+                className='errors'
+                model='forms.join.lastName'
+                show='touched'
+                messages={{
+                  required: 'Required',
+                  maxLength: ' Must be 15 characters or less'
                 }}
               />
-            </Col>
-            <Errors
-              className='errors'
-              model='forms.join.lastName'
-              show='touched'
-              messages={{
-                required: 'Required',
-                maxLength: ' Must be 15 characters or less'
-              }}
-            />
-          </div>
+            </div>
 
-          <div className='titleJoinForm'>
-            <Col sm={6} >
-              <label>Email:</label>
-            </Col>
-            <Col sm={6} >
-              <Control.text
-                placeholder='Enter Your Email'
-                className='input-group input-group-lg'
-                model='.email'
-                validators={{
-                  required,
-                  validEmail: (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val)
+            <div className='titleJoinForm'>
+              <Col sm={4} >
+                <label>Email:</label>
+              </Col>
+              <Col sm={6}>
+                <Control.text
+                  placeholder='Enter Your Email'
+                  className='input-group input-group-lg'
+                  model='.email'
+                  validators={{
+                    required,
+                    validEmail: (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val)
+                  }}
+                />
+              </Col>
+              <Errors
+                className='errors'
+                model='forms.join.email'
+                show='touched'
+                messages={{
+                  required: 'Required',
+                  validEmail: ' Invalid email address'
                 }}
               />
-            </Col>
-            <Errors
-              className='errors'
-              model='forms.join.email'
-              show='touched'
-              messages={{
-                required: 'Required',
-                validEmail: ' Invalid email address'
-              }}
-            />
-          </div>
+            </div>
 
-          <div className='titleJoinForm'>
-            <Col sm={6} >
-              <label>Phone Number:</label>
-            </Col>
-            <Col sm={6} >
-              <Control.text
-                placeholder='Enter Your Phone Number'
-                className='input-group input-group-lg'
-                model='.phoneNumber'
-                validators={{
-                  required,
-                  isNumber,
-                  maxLength: maxLength(15)
+            <div className='titleJoinForm'>
+              <Col sm={4} >
+                <label>Phone Number:</label>
+              </Col>
+              <Col sm={6}>
+                <Control.text
+                  placeholder='Enter Your Phone Number'
+                  className='input-group input-group-lg'
+                  model='.phoneNumber'
+                  validators={{
+                    required,
+                    isNumber,
+                    maxLength: maxLength(15)
+                  }}
+                />
+              </Col>
+                <div>
+                  <Errors
+                    className='errors'
+                    model='forms.join.phoneNumber'
+                    show='touched'
+                    messages={{
+                      required: 'Required',
+                      isNumber: 'Must be a phone number'
+                    }}
+                  />
+              </div>
+            </div>
+
+            <div className='titleJoinForm'>
+              <Col sm={4} >
+                <label>Password:</label>
+              </Col>
+              <Col sm={6}>
+                <Control
+                  placeholder='Enter Your Password'
+                  className='input-group input-group-lg'
+                  type='password'
+                  model='.password'
+                  validators={{
+                    required,
+                    maxLength: maxLength(15)
+                  }}
+                />
+              </Col>
+              <Errors
+                className='errors'
+                model='forms.join.password'
+                show='touched'
+                messages={{
+                  required: 'Required',
+                  maxLength: ' Must be 15 characters or less'
                 }}
               />
-            </Col>
-            <Errors
-              className='errors'
-              model='forms.join.phoneNumber'
-              show='touched'
-              messages={{
-                required: 'Required',
-                isNumber: 'Must be a phone number'
-              }}
-            />
-          </div>
+            </div>
 
-          <div className='titleJoinForm'>
-            <Col sm={6} >
-              <label>Password:</label>
-            </Col>
-            <Col sm={6} >
-              <Control
-                placeholder='Enter Your Password'
-                className='input-group input-group-lg'
-                type='password'
-                model='.password'
-                validators={{
-                  required,
-                  maxLength: maxLength(15)
+            <div className='titleJoinForm'>
+              <Col sm={4} >
+                <label>Confirm Password:</label>
+              </Col>
+              <Col sm={6}>
+                <Control
+                  placeholder='Re-Enter Your Password Here'
+                  className='input-group input-group-lg'
+                  type='password'
+                  model='.confirmPassword'
+                  validators={{
+                    required,
+                    maxLength: maxLength(15)
+                  }}
+                />
+              </Col>
+              <Errors
+                className='errors'
+                model='forms.join.confirmPassword'
+                show='touched'
+                messages={{
+                  required: 'Required',
+                  maxLength: ' Must be 15 characters or less'
                 }}
               />
-            </Col>
-            <Errors
-              className='errors'
-              model='forms.join.password'
-              show='touched'
-              messages={{
-                required: 'Required',
-                maxLength: ' Must be 15 characters or less'
-              }}
-            />
-          </div>
+            </div>
 
-          <div className='titleJoinForm'>
-            <Col sm={6} >
-              <label>Confirm Password:</label>
-            </Col>
-            <Col sm={6} >
-              <Control
-                placeholder='Re-Enter Your Password Here'
-                className='input-group input-group-lg'
-                type='password'
-                model='.confirmPassword'
-                validators={{
-                  required,
-                  maxLength: maxLength(15)
-                }}
+            <Errors
+              model='forms.join'
+              messages={{
+                passwordMatch: 'Password need to match'
+              }}
               />
-            </Col>
-            <Errors
-              className='errors'
-              model='forms.join.confirmPassword'
-              show='touched'
-              messages={{
-                required: 'Required',
-                maxLength: ' Must be 15 characters or less'
-              }}
-            />
+            <div>
+              <Col sm={12} >
+                <button className='submitJoinBtn btn btn-default dropdown-toggle' type='submit'>Add</button>
+                <div id='successful-signup-confirm' />
+              </Col>
+            </div>
           </div>
-
-          <Errors
-            model='forms.join'
-            messages={{
-              passwordMatch: 'Password need to match'
-            }}
-            />
-          <button className='submitJoinBtn' type='submit'>Add</button>
-          <div id='successful-signup-confirm' />
-        </div>
-      </Form>
+        </Form>
+      </div>
     )
   }
 })
