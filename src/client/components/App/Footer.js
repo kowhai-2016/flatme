e938d3f3e36f83e4a4488b2abe458cda7132648b
Quinn-H <quinn.hu618@gmail.com>
2017-02-13 17:20:56 +1300

    fix footer to avoid conflict

--- src/client/components/App/Footer.js
+++ src/client/components/App/Footer.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router'
 const Footer = props => {
   return (
     <div>
-      <nav className='navbar navbar-inverse navbar-fixed-bottom'>
+      <nav className='Footer navbar navbar-inverse navbar-fixed-bottom'>
         <div className='container-fluid'>
           <a href='https://github.com/kowhai-2016/flatme'>
             <img className='git-logo' src='/images/GitHub-Mark-light-32px.png' />
