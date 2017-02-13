3edcb1f9ef6fee77299ea07aaaf323a9dd080854
AnnaUlyanova <a.ulyanova89@gmail.com>
2017-02-14 12:37:23 +1300

    add route to the calendar

--- src/client/components/Flat/Categories.js
+++ src/client/components/Flat/Categories.js
@@ -32,6 +32,13 @@ const Categories = props => {
           <span>Notes</span>
         </Link>
       </div>
+      <div>
+        <Link activeClassName={'active'}
+          to={`/flat/${id}/calendar`}>
+          <img src='/images/paper.svg' width='25px' />
+          <span>Calendar</span>
+        </Link>
+      </div>
     </Navbar>
   )
 }
