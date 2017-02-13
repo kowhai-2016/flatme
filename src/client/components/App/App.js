9b4eed92b698c57afdba0147935604c464ea6184
AnnaUlyanova <a.ulyanova89@gmail.com>
2017-02-13 17:21:44 +1300

    Refactor Footer css

--- src/client/components/App/App.js
+++ src/client/components/App/App.js
@@ -3,6 +3,8 @@ import React from 'react'
 import Navigation from '../../containers/Navigation'
 import Footer from './Footer'
 
+import './style.css'
+
 const App = props => {
   return (
     <div>
--- src/client/components/App/Footer.js
+++ src/client/components/App/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = props => {
   return (
     <div>
-      <nav className='navbar navbar-inverse navbar-bottom'>
+      <nav className='Footer navbar navbar-inverse navbar-bottom'>
         <div className='container-fluid signature' >
           <div><h6>Kowhai&emsp;&emsp;&emsp;&emsp;&emsp;2017</h6></div>
           <a href='https://github.com/kowhai-2016/flatme'>
