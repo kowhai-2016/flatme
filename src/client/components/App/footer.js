5f959af6544ee00af8b7ba757608d62ec3bbf4bb
Amanda <hogan.ans@gmail.com>
2017-02-13 14:49:46 +1300

    footer in working condition

--- src/client/components/App/footer.js
+++ src/client/components/App/footer.js
@@ -3,11 +3,10 @@ import React from 'react'
 const Footer = props => {
   return (
     <div>
-      <nav className='navbar navbar-inverse navbar-fixed-bottom'>
-        <div className='container-fluid'>
-          <a href='https://github.com/kowhai-2016/flatme'>
-            <img className='git-logo center-block' src='/images/GitHub-Mark-light-32px.png' />
-          </a>
+      <nav className='navbar navbar-inverse navbar-bottom'>
+        <div className='container-fluid signature'>
+          <div><h6>Kowhai&emsp;&emsp;&emsp;&emsp;2017</h6></div>
+          <a href='https://github.com/kowhai-2016/flatme'><img className='git-logo center-block' src='/images/GitHub-Mark-light-32px.png' /></a>
         </div>
       </nav>
     </div>
