4a5c21ea70e39d2e11b547bf2ed831e93eb35804
David Christie <davidchristie.nz@gmail.com>
2017-02-14 14:26:22 +1300

    All tests pass

--- test/client/components/App/App.test.js
+++ test/client/components/App/App.test.js
@@ -1,13 +1,20 @@
 /* eslint-env jest */
 
-import { shallow } from 'enzyme'
 import React from 'react'
+import renderer from 'react-test-renderer'
+import { Provider } from 'react-redux'
+import configureStore from 'redux-mock-store'
 
 import App from '../../../../src/client/components/App'
 
-describe('App', () => {
-  test('should render Navigation component', () => {
-    const wrapper = shallow(<App />)
-    expect(wrapper.text().includes('Navigation')).toBe(true)
-  })
+const mockStore = configureStore([])
+
+test('App renders correctly', () => {
+  const store = mockStore({account: {}})
+  const tree = renderer.create(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  ).toJSON()
+  expect(tree).toMatchSnapshot()
 })
