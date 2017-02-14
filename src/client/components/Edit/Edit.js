5dd6cacad07bcdf91a97c4e2ea1678dfac9de871
Quinn-H <quinn.hu618@gmail.com>
2017-02-14 18:31:31 +1300

    user editing done

--- src/client/components/Edit/Edit.js
+++ src/client/components/Edit/Edit.js
@@ -8,19 +8,42 @@ const Edit = React.createClass({
 
   render () {
     return (
+      <div className='row'>
+        <div className='container'>
+        <div className='text-center'>
+          <h1>Update your profile</h1>
+        </div>
       <Form model='forms.edit'
         onSubmit={this.handleSubmit}
         >
-        <label>First name:</label>
-        <Control.text model='.firstName' />
-        <label>Last name:</label>
-        <Control.text model='.lastName' />
-        <label>Phone number:</label>
-        <Control.text model='.phoneNumber' />
-        <label>Email:</label>
-        <Control.text model='.email' />
+        <div className='form-group'>
+        <div className='field'>
+          <label>First name:</label>
+          <Control.text model='.firstName' />
+        </div>
+        </div>
+        <div className='form-group'>
+        <div className='field'>
+          <label>Last name:</label>
+          <Control.text model='.lastName' />
+        </div>
+        </div>
+        <div className='form-group'>
+        <div className='field'>
+          <label>Phone number:</label>
+          <Control.text model='.phoneNumber' />
+        </div>
+        </div>
+        <div className='form-group'>
+        <div className='field'>
+          <label>Email:</label>
+          <Control.text model='.email' />
+        </div>
+        </div>
         <button type='submit'>Update</button>
       </Form>
+      </div>
+      </div>
     )
   }
 })
