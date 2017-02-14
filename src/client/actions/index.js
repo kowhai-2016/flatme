b89df710a7d5ce70c3808a901fabf4649d35d9ee
Jessica Neary <Caneary@hotmail.com>
2017-02-15 11:26:10 +1300

    Readded mysterious disappearing code

--- src/client/actions/index.js
+++ src/client/actions/index.js
@@ -37,6 +37,29 @@ export const fetchFlat = id => {
   }
 }
 
+export const updateUser = fields => {
+  return (dispatch, getState) => {
+    const userId = getState().account.user.id
+    dispatch({
+      userId,
+      type: 'UPDATE_USER_PENDING'
+    })
+    return getAxios().put(`/v1/users/${userId}`, fields)
+     .then(res => {
+       return dispatch({
+         type: 'UPDATE_USER_SUCCESS',
+         fields
+       })
+     })
+     .catch(error => {
+       return dispatch({
+         message: error.message,
+         type: 'UPDATE_USER_FAILURE'
+       })
+     })
+  }
+}
+
 export const fetchUserFlats = id => {
   return dispatch => {
     dispatch({
