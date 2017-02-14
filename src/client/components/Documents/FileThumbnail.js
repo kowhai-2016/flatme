42320027ddee1ebe432c0b2660dbbbc852d3b935
amandahogan <hogan.ans@gmail.com>
2017-02-15 12:28:51 +1300

    show backup image when document is uploaded

--- src/client/components/Documents/FileThumbnail.js
+++ src/client/components/Documents/FileThumbnail.js
@@ -3,9 +3,12 @@ import { Thumbnail } from 'react-bootstrap'
 
 const FileThumbnail = props => {
   const { url, name } = props.document
+  const showBackupImage = event => {
+    event.target.src = 'http://img.freepik.com/free-icon/document-file-hand-drawn-symbol_318-52062.jpg?size=338&ext=jpg'
+  }
   return (
-    <Thumbnail>
-      <img className='img-thumbnail' src={url} />
+    <Thumbnail href={url}>
+      <img className='img-thumbnail' src={url} onError={showBackupImage} />
       <h4>{name}</h4>
     </Thumbnail>
   )
