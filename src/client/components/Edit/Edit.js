08977c9c5d9f2e81bd2b72cff557303dda80273c
Quinn-H <quinn.hu618@gmail.com>
2017-02-15 15:10:37 +1300

    user editing work

--- src/client/components/Edit/Edit.js
+++ src/client/components/Edit/Edit.js
@@ -8,92 +8,95 @@ const Edit = React.createClass({
   onSubmit (values) {
     console.log(values)
     this.props.updateUser(values)
+    window.alert('Change successful applied')
   },
-
   render () {
-    const { user } = this.props
-
     return (
-        <div>
+      <div className='Edit'>
         <div className='text-center'>
           <h1>Update your profile</h1>
         </div>
-      <Form model="forms.account.user"
-        onSubmit={this.onSubmit}
-        >
-        <div className='field'>
-          <label>First name:</label>
-          <Control.text
-            model='.firstName'
-            validators={{
-              maxLength: maxLength(15)
-            }}
-          />
-        <Errors
-          className='errors'
-          model='.firstName'
-          show='touched'
-          messages={{
-            maxLength: 'Must be 15 characters or less'
-          }}
-        />
-        </div>
-
-        <div className='field'>
-          <label>Last name:</label>
-          <Control.text
-            model='.lastName'
-            validators={{
-              maxLength: maxLength(15)
-              }}
-          />
-          <Errors
-            className='errors'
-            model='.lastName'
-            show='touched'
-            messages={{
-              maxLength: 'Must be 15 characters or less'
-            }}
-          />
-        </div>
-
-        <div className='field'>
-          <label>Phone number:</label>
-            <Control.text
-              model='.phoneNumber'
-            />
-            <Errors
-              className='errors'
-              model='.phoneNumber'
-              show='touched'
-              messages={{
-                isNumber: 'Must be Number'
-              }}
-            />
-        </div>
-
-        <div className='field'>
-          <label>Email:</label>
-            <Control.text
-                model='.email'
-                validators={{
-                  validEmail: (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val)
+        <div className='container'>
+          <div className='span12'>
+            <Form model='forms.account.user'
+              onSubmit={this.onSubmit}
+              >
+              <div className='field'>
+                <label>First name:</label>
+                <Control.text
+                  model='.firstName'
+                  validators={{
+                    maxLength: maxLength(15)
                   }}
-              />
-              <Errors
-                className='errors'
-                model='.email'
-                show='touched'
-                messages={{
-                  validEmail: ' Invalid email address'
-                }}
-              />
+                  />
+                <Errors
+                  className='errors'
+                  model='.firstName'
+                  show='touched'
+                  messages={{
+                    maxLength: 'Must be 15 characters or less'
+                  }}
+                  />
+              </div>
+
+              <div className='field'>
+                <label>Last name:</label>
+                <Control.text
+                  model='.lastName'
+                  validators={{
+                    maxLength: maxLength(15)
+                  }}
+                  />
+                <Errors
+                  className='errors'
+                  model='.lastName'
+                  show='touched'
+                  messages={{
+                    maxLength: 'Must be 15 characters or less'
+                  }}
+                  />
+              </div>
+
+              <div className='field'>
+                <label>Phone number:</label>
+                <Control.text
+                  model='.phoneNumber'
+                  />
+                <Errors
+                  className='errors'
+                  model='.phoneNumber'
+                  show='touched'
+                  messages={{
+                    isNumber: 'Must be Number'
+                  }}
+                  />
+              </div>
+
+              <div className='field'>
+                <label>Email:</label>
+                <Control.text
+                  model='.email'
+                  validators={{
+                    validEmail: (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val)
+                  }}
+                    />
+                <Errors
+                  className='errors'
+                  model='.email'
+                  show='touched'
+                  messages={{
+                    validEmail: ' Invalid email address'
+                  }}
+                    />
+              </div>
+              <button type='submit'>Update</button>
+
+              <Control.reset model='forms.account.user' type='reset'>
+                Reset
+              </Control.reset>
+            </Form>
+          </div>
         </div>
-        <button type='submit'>Update</button>
-        <Control.reset  model="forms.account.user" type='reset'>
-          Reset
-        </Control.reset>
-      </Form>
       </div>
     )
   }
