93ecc82d80f1327ec6bd5df025d7d2a27a5c509a
Amanda <hogan.ans@gmail.com>
2017-02-15 16:47:42 +1300

    small Design changes

--- src/client/components/Edit/Edit.js
+++ src/client/components/Edit/Edit.js
@@ -11,93 +11,95 @@ const Edit = React.createClass({
   },
   render () {
     return (
-      <div className='Edit'>
-        <div className='text-center'>
-          <h1>Update your profile</h1>
-        </div>
-        <div className='container'>
-          <div className='span12'>
-            <Form model='forms.account.user'
-              onSubmit={this.onSubmit}
-              >
-              <div className='field'>
-                <label>First name:</label>
-                <Control.text
-                  model='.firstName'
-                  validators={{
-                    maxLength: maxLength(15)
-                  }}
-                  />
-                <Errors
-                  className='errors'
-                  model='.firstName'
-                  show='touched'
-                  messages={{
-                    maxLength: 'Must be 15 characters or less'
-                  }}
-                  />
-              </div>
-
-              <div className='field'>
-                <label>Last name:</label>
-                <Control.text
-                  model='.lastName'
-                  validators={{
-                    maxLength: maxLength(15)
-                  }}
-                  />
-                <Errors
-                  className='errors'
-                  model='.lastName'
-                  show='touched'
-                  messages={{
-                    maxLength: 'Must be 15 characters or less'
-                  }}
-                  />
-              </div>
-
-              <div className='field'>
-                <label>Phone number:</label>
-                <Control.text
-                  model='.phoneNumber'
-                  validators={{
-                    // Temporary: fix this!
-                    isNumber: val => val.length > 0
-                  }}
-                />
-                <Errors
-                  className='errors'
-                  model='.phoneNumber'
-                  show='touched'
-                  messages={{
-                    isNumber: 'Must be Number'
-                  }}
-                />
-              </div>
-
-              <div className='field'>
-                <label>Email:</label>
-                <Control.text
-                  model='.email'
-                  validators={{
-                    validEmail: (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val)
-                  }}
+      <div className='Bg-pict'>
+        <div className='Edit'>
+          <div className='text-center'>
+            <h1>Update your profile</h1>
+          </div>
+          <div className='container'>
+            <div className='span12'>
+              <Form model='forms.account.user'
+                onSubmit={this.onSubmit}
+                >
+                <div className='field'>
+                  <label>First name:</label>
+                  <Control.text
+                    model='.firstName'
+                    validators={{
+                      maxLength: maxLength(15)
+                    }}
                     />
-                <Errors
-                  className='errors'
-                  model='.email'
-                  show='touched'
-                  messages={{
-                    validEmail: ' Invalid email address'
-                  }}
+                  <Errors
+                    className='errors'
+                    model='.firstName'
+                    show='touched'
+                    messages={{
+                      maxLength: 'Must be 15 characters or less'
+                    }}
                     />
-              </div>
-              <button type='submit'>Update</button>
+                </div>
 
-              <Control.reset model='forms.account.user' type='reset'>
-                Reset
-              </Control.reset>
-            </Form>
+                <div className='field'>
+                  <label>Last name:</label>
+                  <Control.text
+                    model='.lastName'
+                    validators={{
+                      maxLength: maxLength(15)
+                    }}
+                    />
+                  <Errors
+                    className='errors'
+                    model='.lastName'
+                    show='touched'
+                    messages={{
+                      maxLength: 'Must be 15 characters or less'
+                    }}
+                    />
+                </div>
+
+                <div className='field'>
+                  <label>Phone number:</label>
+                  <Control.text
+                    model='.phoneNumber'
+                    validators={{
+                      // Temporary: fix this!
+                      isNumber: val => val.length > 0
+                    }}
+                  />
+                  <Errors
+                    className='errors'
+                    model='.phoneNumber'
+                    show='touched'
+                    messages={{
+                      isNumber: 'Must be Number'
+                    }}
+                  />
+                </div>
+
+                <div className='field'>
+                  <label>Email:</label>
+                  <Control.text
+                    model='.email'
+                    validators={{
+                      validEmail: (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val)
+                    }}
+                      />
+                  <Errors
+                    className='errors'
+                    model='.email'
+                    show='touched'
+                    messages={{
+                      validEmail: ' Invalid email address'
+                    }}
+                      />
+                </div>
+                <button type='submit'>Update</button>
+
+                <Control.reset className='btnUpdate' model='forms.account.user' type='reset'>
+                  Reset
+                </Control.reset>
+              </Form>
+            </div>
           </div>
         </div>
       </div>
